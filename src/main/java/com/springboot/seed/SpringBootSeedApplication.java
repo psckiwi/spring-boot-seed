554b5e4b99f8387ecb1a147d5ff7a061ce11d901
gilbert <parksechae@gmail.com>
2018-07-30 15:13:21 +0900

    print run environment

--- src/main/java/com/springboot/seed/SpringBootSeedApplication.java
+++ src/main/java/com/springboot/seed/SpringBootSeedApplication.java
@@ -1,12 +1,39 @@
 package com.springboot.seed;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SpringBootSeedApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootSeedApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(SpringBootSeedApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(SpringBootSeedApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
     }
 }
