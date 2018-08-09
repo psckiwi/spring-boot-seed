33959e7d74b235fae553e117f80fa4880e4dfebf
gilbert <parksechae@gmail.com>
2018-08-09 09:43:01 +0900

    logging (logback)

--- src/main/java/com/springboot/seed/sample/profile/SampleProfileController.java
+++ src/main/java/com/springboot/seed/sample/profile/SampleProfileController.java
@@ -1,5 +1,7 @@
 package com.springboot.seed.sample.profile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,14 @@ import java.util.Arrays;
 @RequestMapping("/sample")
 public class SampleProfileController {
 
+    private final static Logger log = LoggerFactory.getLogger(SampleProfileController.class);
+
     @Autowired
     private Environment env;
 
     @GetMapping("/profile")
     public String getProfile() {
+        log.debug("Active Profiles : {}", Arrays.toString(env.getActiveProfiles()));
         return Arrays.toString(env.getActiveProfiles()) + env.getProperty("application.property.hello");
     }
 }
